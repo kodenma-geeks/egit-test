49a7a7dde3a39197e9efa67b020a269ca4ac2f02
dragonvirgo <norihide.shimatani@gmail.com>
2012-06-09 21:25:53 +0900

    テキストフィールドを追加
    Signed-off-by: dragonvirgo <norihide.shimatani@gmail.com>

--- egit-test/src/shima/android/EgitTestActivity.java
+++ egit-test/src/shima/android/EgitTestActivity.java
@@ -1,7 +1,11 @@
 package shima.android;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EgitTestActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,16 @@ public class EgitTestActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        
+        LinearLayout linear = (LinearLayout)findViewById(R.id.linear);
+ 		TextView titleView = new TextView(this);
+ 		titleView.setText("user-Aが６月９日21時15分に追加修正し、ローカルにコミット");
+ 		titleView.setTextSize(12);
+ 		titleView.setTextColor(Color.BLUE);
+ 		titleView.setBackgroundColor(Color.LTGRAY);
+ 		titleView.setHeight(50);
+ 		titleView.setGravity(Gravity.CENTER);
+ 		linear.addView(titleView);
     }
 }
