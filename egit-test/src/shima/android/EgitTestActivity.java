b10544e71e3b5cf3f0e7db871dd1d68ebeba01df
dragonvirgo <norihide.shimatani@gmail.com>
2012-06-09 20:54:31 +0900

    user-Bがテキストフィールドを追加
    Signed-off-by: dragonvirgo <norihide.shimatani@gmail.com>

--- egit-test/src/shima/android/EgitTestActivity.java
+++ egit-test/src/shima/android/EgitTestActivity.java
@@ -1,7 +1,11 @@
 package shima.android;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class EgitTestActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,15 @@ public class EgitTestActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        LinearLayout linear = (LinearLayout)findViewById(R.id.linearLayout);
+		TextView titleView = new TextView(this);
+		titleView.setText("user-B‚ª‚UŒ‚X“ú‚Q‚O55•ª‚É’Ç‰ÁC³‚µAGitHub‚ÉƒvƒbƒVƒ…");
+		titleView.setTextSize(18);
+		titleView.setTextColor(Color.GREEN);
+		titleView.setBackgroundColor(Color.LTGRAY);
+		titleView.setHeight(120);
+		titleView.setGravity(Gravity.CENTER);
+		linear.addView(titleView);
     }
 }
