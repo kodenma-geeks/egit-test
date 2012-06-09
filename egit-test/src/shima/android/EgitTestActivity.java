180fd8f98b3cc31988c9b03f06f605d948dfb931
dragonvirgo <norihide.shimatani@gmail.com>
2012-06-09 21:59:57 +0900

    追加修正
    Signed-off-by: dragonvirgo <norihide.shimatani@gmail.com>

--- egit-test/src/shima/android/EgitTestActivity.java
+++ egit-test/src/shima/android/EgitTestActivity.java
@@ -14,11 +14,21 @@ public class EgitTestActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        LinearLayout linear = (LinearLayout)findViewById(R.id.linearLayout);
+        LinearLayout linear = (LinearLayout)findViewById(R.id.linear);
 		TextView titleView = new TextView(this);
 		titleView.setText("user-Bが６月９日２０時55分に追加修正し、GitHubにプッシュ");
 		titleView.setTextSize(18);
 		titleView.setTextColor(Color.GREEN);
+		titleView.setBackgroundColor(Color.LTGRAY);
+		titleView.setHeight(120);
+		titleView.setGravity(Gravity.CENTER);
+		linear.addView(titleView);
+		
+        linear = (LinearLayout)findViewById(R.id.linear);
+		titleView = new TextView(this);
+		titleView.setText("user-Bが６月９日22時00、user-Aがプッシュしたものを再度マージした");
+		titleView.setTextSize(18);
+		titleView.setTextColor(Color.RED);
 		titleView.setBackgroundColor(Color.LTGRAY);
 		titleView.setHeight(120);
 		titleView.setGravity(Gravity.CENTER);
